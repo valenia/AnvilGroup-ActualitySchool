8ed8f4dad8bea8462b789458dfb0ddbbb54a44b5
Denis Anfruns <daanfruns@gmail.com>
2021-02-20 19:19:48 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
+                        "<strong>El teu missatge ha estat enviat.</strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
@@ -55,9 +55,9 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
+                            "Ho sentim " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", sembla que el nostre servidor de correu no respon. Siusplau, intenta-ho més tard!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
