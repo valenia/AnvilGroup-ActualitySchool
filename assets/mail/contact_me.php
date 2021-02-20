fe0476746b4d31ffedd598fba394ac8918fcc697
Denis Anfruns <daanfruns@gmail.com>
2021-02-20 19:02:22 +0100

    Update contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -18,7 +18,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'anmieducem@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "Heu rebut un missatge nou del formulari de contacte del vostre lloc web.\n\n"."Aquí en teniu els detalls:\n\nNom: $name\n\nCorreu electrònic: $email_address\n\nTelèfon: $phone\n\nMissatge:\n$message";
+$email_body = "Heu rebut un missatge nou del formulari de contacte del vostre lloc web anvilgroup.cat.\n\n"."Aquí en teniu els detalls:\n\nNom: $name\n\nCorreu electrònic: $email_address\n\nTelèfon: $phone\n\nMissatge:\n$message";
 $headers = "From: noreply@anvilgroup.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
